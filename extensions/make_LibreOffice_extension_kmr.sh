4a04017c0f86a504c11f6f7b37b59d86094c3bf5
sinahm <ahmadi.sina@outlook.com>
2022-04-30 10:27:34 +0100

    kmr dictionary updated

--- extensions/make_LibreOffice_extension_kmr.sh
+++ extensions/make_LibreOffice_extension_kmr.sh
@@ -1,3 +1,3 @@
-makeoxt -d ../kmr/kmr-Latn.aff --dicttype hunspell -l "Kurdish, Northern (Kurmanji)" -t west --publisher "Sina Ahmadi (ahmadi.sina@outlook.com)" --puburl "https://github.com/sinaahmadi/KurdishHunspell" --license ../LICENSE kmr kmr.oxt
+makeoxt -d ../kmr/kmr-Latn.aff --dicttype hunspell -l "Kurdish, Northern (Kurmanji)" -t west --publisher "Sina Ahmadi (ahmadi.sina@outlook.com)" --puburl "https://github.com/sinaahmadi/KurdishHunspell" --license ../LICENSE kmr-Latn kmr.oxt
 
 
